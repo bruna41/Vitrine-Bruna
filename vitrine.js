a2f1ba0bb676f05b8701cd3068002de709b446e2
bruna41 <bruna41bergami@gmail.com>
2022-12-16 19:21:06 -0300

    @bruna41 [Melhorias no código]

---
+++ vitrine.js
@@ -0,0 +1,63 @@
+function mais() {
+    let pontos = document.getElementById('pontinhos')
+    let maisTexto = document.getElementById('leiaMais')
+    let btn = document.getElementById('btnLeiaMais')
+
+    if (pontos.style.display === 'none') {
+        pontos.style.display = 'inline'
+        maisTexto.style.display = 'none'
+        btn.innerHTML = 'Leia Mais'
+    } else {
+        pontos.style.display = 'none'
+        maisTexto.style.display = 'inline'
+        btn.innerHTML = 'Esconder'
+    }
+}
+
+function mais2() {
+    let pontos = document.getElementById('pontinhos2')
+    let maisTexto = document.getElementById('leiaMais2')
+    let btn = document.getElementById('btnLeiaMais2')
+
+    if (pontos.style.display === 'none') {
+        pontos.style.display = 'inline'
+        maisTexto.style.display = 'none'
+        btn.innerHTML = 'Leia Mais'
+    } else {
+        pontos.style.display = 'none'
+        maisTexto.style.display = 'inline'
+        btn.innerHTML = 'Esconder'
+    }
+}
+
+function mais3() {
+    let pontos = document.getElementById('pontinhos3')
+    let maisTexto = document.getElementById('leiaMais3')
+    let btn = document.getElementById('btnLeiaMais3')
+
+    if (pontos.style.display === 'none') {
+        pontos.style.display = 'inline'
+        maisTexto.style.display = 'none'
+        btn.innerHTML = 'Leia Mais'
+    } else {
+        pontos.style.display = 'none'
+        maisTexto.style.display = 'inline'
+        btn.innerHTML = 'Esconder'
+    }
+}
+
+function mais4() {
+    let pontos = document.getElementById('pontinhos4')
+    let maisTexto = document.getElementById('leiaMais4')
+    let btn = document.getElementById('btnLeiaMais4')
+
+    if (pontos.style.display === 'none') {
+        pontos.style.display = 'inline'
+        maisTexto.style.display = 'none'
+        btn.innerHTML = 'Leia Mais'
+    } else {
+        pontos.style.display = 'none'
+        maisTexto.style.display = 'inline'
+        btn.innerHTML = 'Esconder'
+    }
+}
